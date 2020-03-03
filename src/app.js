026339a94078b72f26d5ddf2d25e2d77aa833064
Mahmud <ajaomahmud@gmail.com>
2020-03-03 09:39:20 +0100

    chore: made a root route

--- src/app.js
+++ src/app.js
@@ -3,3 +3,7 @@ import { port } from './config/secret';
 import server from './startup/server';
 
 server.listen(port, () => winston.info(`Running on port ${port}...`));
+
+server.get('/', (req, res) => {
+  res.send('WELCOME TO ECOMMERCE API');
+});
